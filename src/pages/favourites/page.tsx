3eb162f4312de81c153828ea658047aab87fb7ad
e-ghazaryan-1996 <emil.mods@mail.ru>
2024-11-27 15:54:26 +0400

    fix : fixed favorite pages character card

--- src/pages/favourites/page.tsx
+++ src/pages/favourites/page.tsx
@@ -1,3 +1,4 @@
+import PeopleCard from "@/components/people/PeopleCard";
 import useFavoritesStore from "@/hooks/useFavouriteStore";
 
 const Favorites = () => {
@@ -18,15 +19,7 @@ const Favorites = () => {
       </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {favorites.map((person, idx) => (
-          <div
-            key={idx}
-            className="max-w-sm rounded-lg shadow-lg bg-white p-4 transition-transform transform hover:scale-105"
-          >
-            <div className="block text-center text-xl font-semibold text-gray-800 hover:text-blue-500">
-              {person.name}
-            </div>
-            <p className="text-gray-500 text-center">{person.gender}</p>
-          </div>
+          <PeopleCard key={person.url} idx={idx} person={person} />
         ))}
       </div>
     </div>
